e43b99dddd3a99f57dbf4fb8474b4bba29f3a558
as <no Eamil>
2024-03-10 11:47:27 +0800

    第四版本

--- createStatement.ts
+++ createStatement.ts
@@ -3,6 +3,7 @@ import {
   EnrichOrders,
   Invoice,
   Order,
+  Play,
   Plays,
   Statement,
 } from "./entity/rebuildLearnType";
@@ -14,6 +15,7 @@ function createStatement(invoice: Invoice, plays: Plays) {
   statement.totalAmount = totalAmount(statement.orders);
   statement.totalVolumeCredit = totalVolumeCredit(statement.orders);
   return statement;
+
   function totalAmount(orders: EnrichOrders) {
     let result = 0; // 金额
     for (let aOrder of orders) {
@@ -28,46 +30,76 @@ function createStatement(invoice: Invoice, plays: Plays) {
     }
     return result;
   }
-  function amountFor(aOrder: EnrichOrder) {
-    let result = 0;
-    switch (aOrder.play?.type) {
-      case "tragedy": {
-        result = 40000;
-        if (aOrder.audienceNumber > 30) {
-          result += 1000 * (aOrder.audienceNumber - 30);
-        }
-        break;
-      }
-      case "comedy": {
-        result = 30000;
-        if (aOrder.audienceNumber > 20) {
-          result += 10000 + 500 * (aOrder.audienceNumber - 20);
-        }
-        result += 300 * aOrder.audienceNumber;
-        break;
-      }
-      default:
-        throw new Error("未知类型：" + aOrder.play.type);
-    }
-    return result;
-  }
-  function volumeCreditsFor(aOrder: EnrichOrder) {
-    let result = 0;
-    // 计算积分
-    result += Math.max(aOrder.audienceNumber - 30, 0);
-    // 计算额外的积分
-    if (aOrder.play.type === "comedy") {
-      result += Math.floor(aOrder.audienceNumber / 5);
-    }
-    return result;
-  }
   function enrichOrders(aOrder: Order): EnrichOrder {
-    let result = Object.assign(
-      { play: plays[aOrder.playID], amount: 0, volumeCredit: 0 },
-      aOrder
-    );
-    result.amount = amountFor(result);
-    result.volumeCredit = volumeCreditsFor(result);
+    const orderCalc = createOrderCalc(aOrder, plays[aOrder.playID]);
+    let result = Object.assign({}, aOrder) as any as EnrichOrder;
+    result.play = orderCalc.play;
+    result.amount = orderCalc.amount;
+    result.volumeCredit = orderCalc.volumeCredit;
+    return result;
+  }
+}
+
+abstract class OrderCalc {
+  public aOrder: Order;
+  public play: Play;
+
+  protected constructor(aOrder: Order, play: Play) {
+    this.aOrder = aOrder;
+    this.play = play;
+  }
+  abstract get amount(): number;
+  abstract get volumeCredit(): number;
+}
+
+function createOrderCalc(aOrder: Order, play: Play) {
+  switch (play.type) {
+    case "tragedy": {
+      return new TragedyOrderCalc(aOrder, play);
+    }
+    case "comedy": {
+      return new ComedyOrderCalc(aOrder, play);
+    }
+    default:
+      throw new Error("'asd");
+  }
+}
+
+class TragedyOrderCalc extends OrderCalc {
+  constructor(aOrder: Order, play: Play) {
+    super(aOrder, play);
+  }
+  get amount() {
+    let result = 40000;
+    if (this.aOrder.audienceNumber > 30) {
+      result += 1000 * (this.aOrder.audienceNumber - 30);
+    }
+    return result;
+  }
+
+  get volumeCredit() {
+    let result = 0;
+    result += Math.max(this.aOrder.audienceNumber - 30, 0);
+    return result;
+  }
+}
+
+class ComedyOrderCalc extends OrderCalc {
+  constructor(aOrder: Order, play: Play) {
+    super(aOrder, play);
+  }
+  get amount() {
+    let result = 30000;
+    if (this.aOrder.audienceNumber > 20) {
+      result += 10000 + 500 * (this.aOrder.audienceNumber - 20);
+    }
+    result += 300 * this.aOrder.audienceNumber;
+    return result;
+  }
+  get volumeCredit() {
+    let result = 0;
+    result += Math.max(this.aOrder.audienceNumber - 30, 0);
+    result += Math.floor(this.aOrder.audienceNumber / 5);
     return result;
   }
 }
